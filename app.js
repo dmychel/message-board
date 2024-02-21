f2776c9eea45b757e0317e79172e5d5cd76a74eb
dmychel <dmychel@tutanota.com>
2024-02-21 13:07:18 -0500

    created temp arr with objects

--- app.js
+++ app.js
@@ -9,6 +9,20 @@ app.set("view engine", "ejs");
 // requests
 app.listen(3000);
 
+// temp arr
+const messages = [
+  {
+    user: "Admin",
+    message: "Hello.. Is this thing working?",
+    date: new Date(),
+  },
+  {
+    user: "Generic User",
+    message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed",
+    date: new Date(),
+  },
+];
+
 app.get("/", (req, res) => {
   res.render("index");
 });
