f9336aacc1e4060780bb73d19ce0c6dac21d626f
dmychel <dmychel@tutanota.com>
2024-02-21 12:31:07 -0500

    added 404 routing

--- app.js
+++ app.js
@@ -9,3 +9,7 @@ app.listen(3000);
 app.get("/", (req, res) => {
   res.sendFile("./views/index.html", { root: __dirname });
 });
+
+app.use((req, res) => {
+  res.sendFile("./views/404.html", { root: __dirname });
+});
