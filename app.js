713927fa3b2e9573f45756e3bfdaad711bbd213f
dmychel <dmychel@tutanota.com>
2024-02-21 12:43:05 -0500

    set view engine to ejs and render index

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const express = require("express");
 // app
 const app = express();
 
+// engine
+app.set("view engine", "ejs");
+
 // requests
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  res.sendFile("./views/index.html", { root: __dirname });
+  res.render("index");
 });
 
 app.use((req, res) => {
