4e11df962b932ab3993515ea6eab4230c4b2a65d
dmychel <dmychel@tutanota.com>
2024-02-22 14:29:42 -0500

    Bug fix
    renamed Message model to Post.
    Fixed dbURI url to send models to the correct database.

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const Post = require("./models/messages");
 
 // app
 const app = express();
 
+// css
+app.use(express.static(__dirname + "/public"));
+
 // mongodb connectivity
 const dbURI =
-  "mongodb+srv://admin:Livelife123@message-board.9yyxmzd.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://admin:Livelife123@message-board.9yyxmzd.mongodb.net/forum?retryWrites=true&w=majority";
 mongoose
   .connect(dbURI)
   .then((result) => app.listen(3000), console.log("connected to db"))
   .catch((err) => console.log(err));
 
-// css
-app.use(express.static(__dirname + "/public"));
-
 // engine
 app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  const post = new Post({
+    user: "Admin",
+    body: "Hello is this thing working -from mongodb",
+  });
+  post.save().then((result) => {
+    res.send(result).catch((err) => console.log(err));
+  });
+});
 
 app.get("/", (req, res) => {
   const messages = [
