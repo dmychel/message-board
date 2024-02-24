9158384c2fffe0cb7e6760056ec57625e6376fc7
dmychel <dmychel@tutanota.com>
2024-02-24 01:44:07 -0500

    added property to schema

--- models/feedback_model.js
+++ models/feedback_model.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-
 const feedbackSchema = new Schema(
   {
+    name: {
+      type: String,
+    },
     email: {
       type: String,
     },
